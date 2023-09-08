1d4434ca94a9aa3629a1889b39d4334cbd47a359
Waitara <janewaitara99@gmail.com>
2023-09-08 22:25:16 +0300

    Initialized app

---
+++ app/src/main/java/com/mumbicodes/composelayouts/ui/theme/Color.kt
@@ -0,0 +1,8 @@
+package com.mumbicodes.composelayouts.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val spotifyGreen = Color(0xFF1DB954)
+val GreyDark = Color(0xFF040306)
+val GreyNeutral = Color(0xFFB3B3B3)
+val GreyLight = Color(0xFFFFFFFF)
---
+++ app/src/main/java/com/mumbicodes/composelayouts/ui/theme/Theme.kt
@@ -0,0 +1,44 @@
+package com.mumbicodes.composelayouts.ui.theme
+
+import android.app.Activity
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.darkColorScheme
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.graphics.toArgb
+import androidx.compose.ui.platform.LocalView
+import androidx.core.view.WindowCompat
+
+private val DarkColorScheme = darkColorScheme(
+    primary = spotifyGreen,
+    background = GreyDark,
+    onBackground = GreyLight,
+    onPrimary = GreyNeutral
+)
+
+@Composable
+fun ComposelayoutsTheme(
+    darkTheme: Boolean = isSystemInDarkTheme(),
+    // Dynamic color is available on Android 12+
+    content: @Composable () -> Unit
+) {
+    val colorScheme = when {
+        darkTheme -> DarkColorScheme
+        else -> DarkColorScheme
+    }
+    val view = LocalView.current
+    if (!view.isInEditMode) {
+        SideEffect {
+            val window = (view.context as Activity).window
+            window.statusBarColor = colorScheme.background.toArgb()
+            WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = darkTheme
+        }
+    }
+
+    MaterialTheme(
+        colorScheme = colorScheme,
+        typography = Typography,
+        content = content
+    )
+}
