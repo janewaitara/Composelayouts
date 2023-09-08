6a84fb876773c7f6dc2dc10da9b6cbdaf3c38579
Waitara <janewaitara99@gmail.com>
2023-09-09 01:27:01 +0300

    ConstraintLayout: Added a show component

--- app/src/main/java/com/mumbicodes/composelayouts/ui/theme/Shape.kt
+++ app/src/main/java/com/mumbicodes/composelayouts/ui/theme/Shape.kt
@@ -5,6 +5,7 @@ import androidx.compose.material3.Shapes
 import androidx.compose.ui.unit.dp
 
 val Shapes = Shapes(
+    extraSmall = RoundedCornerShape(4.dp),
     small = RoundedCornerShape(8.dp),
     medium = RoundedCornerShape(16.dp),
     large = RoundedCornerShape(24.dp)
