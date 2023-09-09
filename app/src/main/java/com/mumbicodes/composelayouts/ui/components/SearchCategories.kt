d1c23d32b10ba027661dc78adf5db9ec97580699
Waitara <janewaitara99@gmail.com>
2023-09-09 13:22:36 +0300

    Box: Added a searchCategory composable

---
+++ app/src/main/java/com/mumbicodes/composelayouts/ui/components/SearchCategories.kt
@@ -0,0 +1,71 @@
+package com.mumbicodes.composelayouts.ui.components
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Card
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.rotate
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.mumbicodes.composelayouts.model.Podcast
+import com.mumbicodes.composelayouts.model.podcasts
+import com.mumbicodes.composelayouts.ui.theme.ComposelayoutsTheme
+import com.mumbicodes.composelayouts.ui.theme.categoryColors
+
+@Composable
+fun SearchCategories(
+    modifier: Modifier = Modifier,
+    podcast: Podcast,
+) {
+    Box(
+        modifier = modifier
+            .size(160.dp, height = 136.dp)
+            .background(color = categoryColors.random())
+    ) {
+        Text(
+            modifier = Modifier
+                .padding(12.dp),
+            text = podcast.category,
+            color = MaterialTheme.colorScheme.onPrimary,
+            style = MaterialTheme.typography.titleSmall,
+            overflow = TextOverflow.Ellipsis,
+            maxLines = 1
+        )
+
+        Card(
+            modifier = Modifier
+                .align(Alignment.BottomEnd)
+                .rotate(32f)
+                .offset((16).dp),
+            shape = MaterialTheme.shapes.small
+        ) {
+
+            Image(
+                modifier = Modifier
+                    .size(80.dp),
+                painter = painterResource(id = podcast.image),
+                contentDescription = "${podcast.podcastTitle} banner",
+                contentScale = ContentScale.Crop
+            )
+        }
+    }
+}
+
+@Preview
+@Composable
+fun searchCategoryPreview() {
+    ComposelayoutsTheme {
+        SearchCategories(podcast = podcasts.first())
+    }
+}
--- app/src/main/java/com/mumbicodes/composelayouts/ui/theme/Color.kt
+++ app/src/main/java/com/mumbicodes/composelayouts/ui/theme/Color.kt
@@ -7,3 +7,12 @@ val GreyDarker = Color(0xFF040306)
 val GreyDark = Color(0xFF121212)
 val GreyNeutral = Color(0xFFB3B3B3)
 val GreyLight = Color(0xFFFFFFFF)
+
+val categoryColors = listOf<Color>(
+    Color(0xFFB284BE),
+    Color(0xFFDE3163),
+    Color(0xFFFF00FF),
+    Color(0xFF00A86B),
+    Color(0xFF40E0D0),
+    Color(0xFFF8DE7E),
+)
